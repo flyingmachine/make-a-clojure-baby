a42444a86941bc6590cca5d0b4ec6f6a46745a95
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-03-11 19:17:37 -0400

    example app

---
+++ ShiverMeTimbers/ShiverMeTimbers.java
@@ -0,0 +1,12 @@
+/**
+ * This program displays a greeting from the authors.
+ * @version 1.20 2004-02-28
+ * @author Cay Horstmann
+ */
+public class ShiverMeTimbers
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Shiver me timbers!!!");
+    }
+}
