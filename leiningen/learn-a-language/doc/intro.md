21264c4945f85394f255cd16282728b202e0b207
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-03-16 21:53:40 -0400

    adding a simple leiningen porject

---
+++ leiningen/learn-a-language/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to learn-a-language
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ leiningen/learn-a-language/test/learn_a_language/core_test.clj
@@ -0,0 +1,7 @@
+(ns learn-a-language.core-test
+  (:use clojure.test
+        learn-a-language.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
