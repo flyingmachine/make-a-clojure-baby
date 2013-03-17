9e102a7c5b3caa4e44a31cc89d751d6d61bdaf4c
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-03-17 08:48:44 -0400

    move

--- leiningen/learn-a-language/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to learn-a-language
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- leiningen/learn-a-language/test/learn_a_language/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns learn-a-language.core-test
-  (:use clojure.test
-        learn-a-language.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
