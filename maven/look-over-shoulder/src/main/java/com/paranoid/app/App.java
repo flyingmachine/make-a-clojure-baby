3b59d661371a8adba0231e628c5c74ae7221df49
Daniel Higginbotham <daniel@flyingmachinestudios.com>
2013-04-01 18:45:54 -0400

    break up text over multiple lines

--- maven/look-over-shoulder/src/main/java/com/paranoid/app/App.java
+++ maven/look-over-shoulder/src/main/java/com/paranoid/app/App.java
@@ -7,9 +7,11 @@ public class App
     {
         Random r = new Random();
         if (r.nextInt(3) == 0) {
-            System.out.println( "Oh dear lord! You were right! THEY are finally here to get you! Run for your life! Stop reading right now and dive out the window or something!" );
+            System.out.println("Great squid of Madrid! You were right! " +
+                               "THEY are finally here to get you! Run for your life! " +
+                               "Stop reading right now and dive out the window or something!");
         } else {
-            System.out.println( "Oh thank God, there's nothing there... yet." );
+            System.out.println("Oh thank God, there's nothing there... yet.");
         }
     }
 }
